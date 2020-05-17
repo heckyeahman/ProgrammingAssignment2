99f05fcb8b1c46d1048f6ecd30af73e832b38afe
Iris Heck <irisherrandgirl@hotmail.com>
2020-05-17 16:43:23 -0700

    Edited the first function and added an overall description.

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions evaluate a matrix, cache the value of the inverse, and report it.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
